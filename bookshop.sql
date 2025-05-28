8f63f242ed4a3467b98a12b55c8a907ff32ff876
B-Halalipour <b.halalipour@gmail.com>
2025-05-28 05:12:04 +0100

    task-1 complete

--- bookshop.sql
+++ bookshop.sql
@@ -1,3 +1,11 @@
+-- books associated with the author
 SELECT books.title, authors.author_name
 FROM books
 JOIN authors ON books.author_id = authors.author_id;
+
+-- authors not associated with books
+
+SELECT authors.author_name
+FROM authors
+LEFT JOIN books on books.author_id = authors.author_id
+WHERE books.author_id IS NULL;
